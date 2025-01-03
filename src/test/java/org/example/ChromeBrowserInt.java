12625cea55935ad5588f4bb65e62157ee7b33534
Chaity Paul <chaity.paul193@gmail.com>
2025-01-04 03:37:24 +0600

    Chomre Browser

---
+++ src/test/java/org/example/ChromeBrowserInt.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class ChromeBrowserInt {
+    WebDriver driver;
+    @BeforeSuite
+    public void startChromeBrowser(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void openURL() throws InterruptedException {
+        driver.get("https://chaldal.com/");
+        Thread.sleep(5000);
+    }
+    @AfterSuite
+    public void closeChromeBrowser(){
+        driver.close();
+    }
+}
