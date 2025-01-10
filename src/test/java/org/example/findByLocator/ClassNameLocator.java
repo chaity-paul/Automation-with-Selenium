b3b434f16a0f0209e3a43a75ed4e8edc00325983
Chaity Paul <chaity.paul193@gmail.com>
2025-01-11 02:13:29 +0600

    Example of className Locator

---
+++ src/test/java/org/example/findByLocator/ClassNameLocator.java
@@ -0,0 +1,33 @@
+package org.example.findByLocator;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class ClassNameLocator {
+    WebDriver driver;
+    @BeforeSuite
+    public void startBrowser(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void openUrl() throws InterruptedException {
+        driver.get("https://www.tutorialspoint.com/selenium/practice/text-box.php");
+        Thread.sleep(3000);
+
+        WebElement home = driver.findElement(By.className("btn"));
+        home.click();
+        Thread.sleep(5000);
+    }
+    @AfterSuite
+    public void closeBrowser(){
+        driver.quit();
+    }
+}
