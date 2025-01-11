22bf5d5b293d580235128936c303cbfc6884673d
Chaity Paul <chaity.paul193@gmail.com>
2025-01-11 18:20:33 +0600

    TestNG annotations to demonstrate the execution flow

---
+++ src/test/java/org/example/assignments/Assignment2_Part2.java
@@ -0,0 +1,46 @@
+package org.example.assignments;
+
+import org.testng.annotations.*;
+
+public class Assignment2_Part2 {
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("Before Suite");
+    }
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("After Suite");
+    }
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("Before Test");
+    }
+    @AfterTest
+    public void afterTest() {
+        System.out.println("After Test");
+    }
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("Before Class");
+    }
+    @AfterClass
+    public void afterClass() {
+        System.out.println("After Class");
+    }
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Before Method");
+    }
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("After Method");
+    }
+    @Test(priority = 0)
+    public void test1(){
+        System.out.println("test1");
+    }
+    @Test(priority = 1)
+    public void test2(){
+        System.out.println("test2");
+    }
+}
