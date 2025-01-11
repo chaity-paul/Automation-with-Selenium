7e84cf88cc6ae8ec18f85b8d542fe117bc587be6
Chaity Paul <chaity.paul193@gmail.com>
2025-01-11 18:16:11 +0600

    Automation Script for using headless browser and retrive the page title

---
+++ src/test/java/org/example/assignments/Assignment2_Part1.java
@@ -0,0 +1,56 @@
+package org.example.assignments;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Assignment2_Part1 {
+    String url = "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php";
+    String browser = System.getProperty("browser", "chrome");
+    WebDriver driver;
+    @BeforeSuite
+    public void startBrowser(){
+        if(browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+
+            driver = new ChromeDriver(options);
+            //driver.manage().window().maximize();
+        }
+        else if(browser.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+
+            driver = new FirefoxDriver(options);
+            //driver.manage().window().maximize();
+        }
+        else{
+            WebDriverManager.edgedriver().setup();
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("--headless");
+
+            driver = new EdgeDriver(options);
+            //driver.manage().window().maximize();
+        }
+    }
+    @Test
+    public void openUrl() throws InterruptedException {
+        driver.get(url);
+        System.out.println(driver.getTitle());
+        //Thread.sleep(3000);
+    }
+    @AfterSuite
+    public void closeBrowser(){
+        driver.close();
+    }
+}
